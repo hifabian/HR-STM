efe3f1b707debe1a973d103bb3f72dfa3a0b3403
Fabian <hifabian@ethz.ch>
2019-08-03 22:53:19 +0200

    Fixed curcial bug: Derivative of WF were in wrong order

--- hrstm_tools/cp2k_grid_matrix.py
+++ hrstm_tools/cp2k_grid_matrix.py
@@ -199,8 +199,8 @@ class Cp2kGridMatrix:
         interp = Interpolator(self.reg_grid, self.wfn_matrix[ispin][iene])
         self._wfn[0] = interp(*self.grids[igrid])
         if self.norbs_tip:
-            self._wfn[1] = interp.gradient(*self.grids[igrid],1)
-            self._wfn[2] = interp.gradient(*self.grids[igrid],2)
-            self._wfn[3] = interp.gradient(*self.grids[igrid],3)
+            self._wfn[1] = interp.gradient(*self.grids[igrid],2)
+            self._wfn[2] = interp.gradient(*self.grids[igrid],3)
+            self._wfn[3] = interp.gradient(*self.grids[igrid],1)
         self._cgrid, self._cspin, self._cene = itupel
         return self._wfn
